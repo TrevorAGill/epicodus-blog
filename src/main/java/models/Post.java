8513eb3942f59f9154030e9a5f2e2fe149ace011
Trevor Gill <trevor.a.gill@gmail.com>
2017-08-08 18:39:27 -0700

    add functionality for tracking date/time of post and getting post by ID

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,13 +1,54 @@
 package models;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Post {
     private String content;
+    private static ArrayList<Post> instances = new ArrayList<>();
+    private boolean published; //i’m new
+    private LocalDateTime createdAt;
+    private int id;
 
     public Post (String content){
         this.content = content;
+        this.published = false; //also new
+        this.createdAt = LocalDateTime.now();
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    //method to use use constructor to create a new post in order to DRY test code
+    public static Post newPost() {
+        Post newPost = new Post("this is a test");
+        return newPost;
+    }
+
+    public static Post findById(int id){
+        return instances.get(id-1);
     }
 
     public String getContent() {
         return content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static ArrayList<Post> getAll(){
+        return instances;
+    }
+
+    public static void clearAllPosts(){
+        instances.clear();
+    }
+
+    public boolean getPublished(){ //new too
+        return this.published;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 }
--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+
+import static models.Post.newPost;
 import static org.junit.Assert.*;
 
 public class PostTest {
@@ -25,6 +28,39 @@ public class PostTest {
     public void PostInstantiatesWithContent_true() throws Exception {
         Post post = new Post("Day 1: Intro");
         assertEquals("Day 1: Intro", post.getContent());
+    }
 
+    @Test
+    public void getPublished_isFalseAfterInstantiation_false() throws Exception {
+        Post myPost = new Post("Day 1: Intro");
+        assertEquals(false, myPost.getPublished()); //should never start as published
+    }
+
+    @Test
+    public void getCreatedAt_instantiatesWithCurrentTime_today() throws Exception {
+        Post myPost = newPost();
+        assertEquals(LocalDateTime.now().getDayOfWeek(),myPost.getCreatedAt().getDayOfWeek());
+    }
+
+    @Test
+    public void getId_postsInstantiateWithAnID_1() throws Exception{
+        Post.clearAllPosts();  // Remember, the test will fail without this line! We need to empty leftover Posts from previous tests!
+        Post myPost = newPost();
+        assertEquals(1, myPost.getId());
+    }
+
+    @Test
+    public void findReturnsCorrectPost() throws Exception {
+        Post.clearAllPosts();
+        Post myPost = newPost();
+        assertEquals(1, Post.findById(myPost.getId()).getId());
+    }
+
+    @Test
+    public void findReturnsCorrectPostWhenMoreThanOnePostExists() throws Exception {
+        Post.clearAllPosts();
+        Post post = newPost();
+        Post otherPost = new Post("How to pair successfully");
+        assertEquals(2, Post.findById(otherPost.getId()).getId());
     }
 }
