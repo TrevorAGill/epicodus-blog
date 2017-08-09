348f1568b0222ad91bd5d09c12cda5747c0f2f96
Trevor Gill <trevor.a.gill@gmail.com>
2017-08-08 23:06:30 -0700

    this is a nightmare

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,26 +14,25 @@ public class App {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             ArrayList<Post> posts = Post.getAll();
-//            model.put("posts", Post.getAll());
+            model.put("posts", Post.getAll());
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/posts/:id", (req, res) -> {
+//        get("/posts/:id", (request, response) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            int idOfPostToFind = Integer.parseInt(request.params("id"));
+//            Post foundPost = Post.findById(idOfPostToFind);
+//            model.put("post", foundPost);
+//            return new ModelAndView(model, "post-detail.hbs");
+//        }, new HandlebarsTemplateEngine());
+
+        get("/posts/new", (request, response) -> { //URL to make new post on POST route
             Map<String, Object> model = new HashMap<>();
-            int idOfPostToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
-            Post foundPost = Post.findById(idOfPostToFind); //use it to find post
-            model.put("post", foundPost); //add it to model for template to display
-            return new ModelAndView(model, "post-detail.hbs"); //individual post page.
+            return new ModelAndView(model, "newpost-form.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/posts/new", (request,response)-> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model,"newpost-form");
-        }, new HandlebarsTemplateEngine());
-
-        //route that makes new post
-        post("/posts/new", (request, response) -> { //URL to make new post on POST route
-            Map<String, Object> model = new HashMap<>();
+        post("/posts/new", (request,response)-> {
+            Map<String, Object> model = new HashMap<String, Object>();
             String name = request.queryParams("name");
             Post newPost = new Post(name);
             model.put("post", newPost);
